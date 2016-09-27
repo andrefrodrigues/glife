a01f034c075245bd5617de7ba3667ee2690ae25d
andrefrodrigues <andrefrodrigues21@gmail.com>
2016-09-27 11:50:39 +0100

    completed main

--- main.c
+++ main.c
@@ -3,14 +3,26 @@
 int main(int argc,char *argv[]){
     GameConfig* gc = game_config_new_from_cli(argc,argv);
     if(gc!=NULL){
-    printf("%zu\n",game_config_get_generations(gc));
-    printf("debug mode is: %d\n",gc->debug);
-    printf("silent mode is: %d\n",gc->silent);
+
     Game* g = game_new();
-    game_parse_board(g,gc);
-    game_print_board(g);
-    game_tick(g);
-    game_print_board(g);
+    if( !game_parse_board(g,gc)){
+        int current;
+        if(!gc->silent){
+            printf("Generation 0:\n");
+            game_print_board(g);
+        }
+
+        for(current=1; current < gc->generations && !game_tick(g); current++){
+            if(gc->debug){
+                printf("Generation %d:\n",current);
+                game_print_board(g);
+            }
+        }
+        if(!gc->silent){
+            printf("Generation %d:\n",current);
+            game_print_board(g);
+        }
+    }
     }
     return 0;
 }
